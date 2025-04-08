b67a2c35bec74e50b3ecbc4ffdc9282b519dc680
abhish-1 <abhishekkant135@gmail.com>
2025-04-09 01:39:41 +0530

    home pg updated

--- app/(main)/interview/page.jsx
+++ app/(main)/interview/page.jsx
@@ -14,6 +14,7 @@ export default async function InterviewPrepPage() {
         </h1>
       </div>
       <div className="space-y-6">
+        
         <StatsCards assessments={assessments} />
         <PerformanceChart assessments={assessments} />
         <QuizList assessments={assessments} />
